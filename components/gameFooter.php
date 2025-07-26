1e7ff063f43c3e7bdecf2b02a431ce742a771822
Axel Rojas <axelkevinagustinrojas@gmail.com>
2025-07-26 10:16:38 -0300

    Link al repo en el footer

--- components/gameFooter.php
+++ components/gameFooter.php
@@ -3,7 +3,8 @@ function renderGameFooter()
 {
 ?>
     <footer>
-        <p>Axel Rojas | UNPSJB</p>
+        <a href="https://github.com/AxelRojas-hub/finalLaboratorio" target="_blank" rel="noopener noreferrer">Repositorio en GitHub</a>
+        <p>Axel Rojas | UNPSJB | Julio 2025</p>
         <p>Final Regular Laboratorio de programación y Lenguajes</p>
     </footer>
 <?php
